f5770c20332a7e194bc35471a99b6ce2961c23b6
Sung Kang <sungjk@msn.com>
2016-02-18 10:52:34 -0800

    new test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname,'client')));
 
