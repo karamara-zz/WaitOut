ca4b44ba85ebbf608af9a74ddd62a42c0ac2edb2
Sung Kang <sungjk@msn.com>
2016-04-21 16:34:15 -0700

    added some note on route

--- server/config/routes.js
+++ server/config/routes.js
@@ -5,8 +5,9 @@ module.exports = function(app){
 
 	app.post('/direction', function(req,res){
 		direction.getDirection(req,res)
-
 	})
+
+	// routes for counting how many times the request has been made.
 	app.put('/counter', function(req, res){
 		counter.visit(req,res);
 	})
