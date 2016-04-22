477fce4f47060ca4b07011058db6c8e6b8c77320
Sung Kang <sungjk@msn.com>
2016-04-21 21:07:38 -0700

    still working on new schemas

--- server/models/trip.js
+++ server/models/trip.js
@@ -4,6 +4,14 @@ var TripSchema = new mongoose.Schema({
 		type: mongoose.Schema.ObjectId,
 		ref: "User",
 		required: true,
+	},
+	direcitons: [{
+		type: mongoose.Schema.ObjectId,
+		ref:"Direction",
+	}],
+	name: {
+		type: String,
+		required: true,
 	}
 })
 var Trip = mongoose.model("Trip", TripSchema);
